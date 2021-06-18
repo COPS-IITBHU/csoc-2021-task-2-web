53d58ffd6e9682d9a1a15452ef68923ec6fd30cc
NaveenKumar <naveenkumar12062002@gmail.com>
2021-06-18 18:34:22 +0530

    Completed all Tasks v1.0.0

--- src/init.js
+++ src/init.js
@@ -1,20 +1,55 @@
-import axios from 'axios';
-const API_BASE_URL = 'https://todo-app-csoc.herokuapp.com/';
+import axios from "axios";
+import { newElement } from "./main";
+const API_BASE_URL = "https://todo-app-csoc.herokuapp.com/"; // backend server deployed on heroku
 
-function getTasks() {
+
+
+
+
+
+function fetchTheTasks() {
     /***
      * @todo Fetch the tasks created by the user and display them in the dom.
      */
+    iziToast.info({
+        title: "Info",
+        message: "Loading all todos"
+    });
+    axios({
+        headers: {
+            Authorization: "Token " + localStorage.getItem("token")
+        },
+        url: API_BASE_URL + "todo/",
+        method: "get"
+    }).then(function (response) {
+        const { data } = response;
+
+
+        for (let task of data) {
+            const taskNumber = task.id;
+            const Textintodo = task.title;
+            newElement( Textintodo, taskNumber);
+            
+        }
+        iziToast.destroy();
+    });
 }
+
+
+
+
 
 axios({
     headers: {
-        Authorization: 'Token ' + localStorage.getItem('token'),
+        Authorization: "Token " + localStorage.getItem("token")
     },
-    url: API_BASE_URL + 'auth/profile/',
-    method: 'get',
-}).then(function({data, status}) {
-  document.getElementById('avatar-image').src = 'https://ui-avatars.com/api/?name=' + data.name + '&background=fff&size=33&color=007bff';
-  document.getElementById('profile-name').innerHTML = data.name;
-  getTasks();
-})
+    url: API_BASE_URL + "auth/profile/",
+    method: "get"
+}).then( function ({ data }) {
+    document.getElementById("avatar-image").src =
+        "https://ui-avatars.com/api/?name=" + data.name + "&background=fff&size=33&color=007bff";
+    document.getElementById("profile-name").innerHTML = data.name;
+
+
+    fetchTheTasks();
+});
--- src/main.js
+++ src/main.js
@@ -1,109 +1,248 @@
-import axios from 'axios';
-function displaySuccessToast(message) {
-    iziToast.success({
-        title: 'Success',
-        message: message
-    });
-}
+import axios from "axios";
+const API_BASE_URL = "https://todo-app-csoc.herokuapp.com/";
 
-function displayErrorToast(message) {
-    iziToast.error({
-        title: 'Error',
-        message: message
-    });
-}
-
-function displayInfoToast(message) {
-    iziToast.info({
-        title: 'Info',
-        message: message
-    });
-}
-
-const API_BASE_URL = 'https://todo-app-csoc.herokuapp.com/';
-
+// function for logout
 function logout() {
-    localStorage.removeItem('token');
-    window.location.href = '/login/';
+    localStorage.removeItem("token");
+    window.location.href = "/login/";
 }
 
-function registerFieldsAreValid(firstName, lastName, email, username, password) {
-    if (firstName === '' || lastName === '' || email === '' || username === '' || password === '') {
-        displayErrorToast("Please fill all the fields correctly.");
+// function to display iziToast on Error
+function ErrorToast(message) {
+    iziToast.error({
+        title: "Error",
+        message: message
+    });
+}
+
+// function to display iziToast for showing Information
+function InformationToast(message) {
+    iziToast.info({
+        title: "Info",
+        message: message
+    });
+}
+
+
+
+
+// function to check the authenticity of the entered fields
+function CheckRegisteredFields(firstName, lastName, email, username, password) {
+    if (firstName === "" || lastName === "" || email === "" || username === "" || password === "") {
+        ErrorToast("Please fill details correctly !!!");
         return false;
     }
-    if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))) {
-        displayErrorToast("Please enter a valid email address.")
+    if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+        ErrorToast("Please enter a valid email address.");
         return false;
     }
     return true;
 }
 
-function register() {
-    const firstName = document.getElementById('inputFirstName').value.trim();
-    const lastName = document.getElementById('inputLastName').value.trim();
-    const email = document.getElementById('inputEmail').value.trim();
-    const username = document.getElementById('inputUsername').value.trim();
-    const password = document.getElementById('inputPassword').value;
+// function to check the authenticity for login
+function CheckLoginValid(username, password) {
+    if (username === "" || password === "") {
+        ErrorToast("Please fill details correctly !!!");
+        return false;
+    }
+    return true;
+}
 
-    if (registerFieldsAreValid(firstName, lastName, email, username, password)) {
-        displayInfoToast("Please wait...");
+// function to register the new user
+function register() {
+    const firstName = document.getElementById("inputFirstName").value.trim();
+    const lastName = document.getElementById("inputLastName").value.trim();
+    const email = document.getElementById("inputEmail").value.trim();
+    const username = document.getElementById("inputUsername").value.trim();
+    const password = document.getElementById("inputPassword").value;
+
+    if (CheckRegisteredFields(firstName, lastName, email, username, password)) {
+        InformationToast("Please wait a little...");
 
         const dataForApiRequest = {
-            name: firstName + " " + lastName,
+            name: firstName +" "+ lastName,
             email: email,
             username: username,
             password: password
-        }
+        };
 
         axios({
-            url: API_BASE_URL + 'auth/register/',
-            method: 'post',
-            data: dataForApiRequest,
-        }).then(function({data, status}) {
-          localStorage.setItem('token', data.token);
-          window.location.href = '/';
-        }).catch(function(err) {
-          displayErrorToast('An account using same email or username is already created');
+            url: API_BASE_URL + "auth/register/",
+            method: "post",
+            data: dataForApiRequest
         })
+            .then(function ({ data, status }) {
+                localStorage.setItem("token", data.token);
+                window.location.href = "/";
+            })
+            .catch(function (err) {
+                ErrorToast("An account using same email or username is already created");
+            });
     }
 }
 
+// function to login 
 function login() {
-    /***
-     * @todo Complete this function.
-     * @todo 1. Write code for form validation.
-     * @todo 2. Fetch the auth token from backend and login the user.
-     */
+    const username = document.getElementById("inputUsername").value.trim();
+    const password = document.getElementById("inputPassword").value;
+    if (CheckLoginValid(username, password)) {
+        InformationToast("Please wait...");
+        axios({
+            url: API_BASE_URL + "auth/login/",
+            method: "post",
+            data: { username, password }
+        })
+            .then(function ({ data, status }) {
+                localStorage.setItem("token", data.token);
+                window.location.href = "/";
+            })
+            .catch(function (err) {
+                ErrorToast("Either username or password is incorrect.");
+            });
+    }
 }
 
+// function to add the task in todo 
 function addTask() {
-    /**
-     * @todo Complete this function.
-     * @todo 1. Send the request to add the task to the backend server.
-     * @todo 2. Add the task in the dom.
-     */
+    const todoText = document.querySelector(".todo-add-task input").value.trim();
+
+    if (!todoText) {
+        return;
+    }
+    axios({
+        headers: {
+            Authorization: "Token " + localStorage.getItem("token")
+        },
+        url: API_BASE_URL + "todo/create/",
+        method: "post",
+        data: { title: todoText }
+    })
+        .then(function (response) {
+            axios({
+                headers: {
+                    Authorization: "Token " + localStorage.getItem("token")
+                },
+                url: API_BASE_URL + "todo/",
+                method: "get"
+            }).then(function ({ data, status }) {
+                const newTodo = data[data.length - 1];
+                const taskNo = newTodo.id;
+                newElement(todoText, taskNo);
+            });
+        })
+        .catch(function (err) {
+            ErrorToast("An error occurred");
+        });
 }
 
+
+// function of edit the task in todo 
 function editTask(id) {
-    document.getElementById('task-' + id).classList.add('hideme');
-    document.getElementById('task-actions-' + id).classList.add('hideme');
-    document.getElementById('input-button-' + id).classList.remove('hideme');
-    document.getElementById('done-button-' + id).classList.remove('hideme');
+    document.getElementById("task-" + id).classList.add("hideme");
+    document.getElementById("task-actions-" + id).classList.add("hideme");
+    document.getElementById("input-button-" + id).classList.remove("hideme");
+    document.getElementById("done-button-" + id).classList.remove("hideme");
 }
 
+
+// function to delete the task from todo 
 function deleteTask(id) {
-    /**
-     * @todo Complete this function.
-     * @todo 1. Send the request to delete the task to the backend server.
-     * @todo 2. Remove the task from the dom.
-     */
+    axios({
+        headers: {
+            Authorization: "Token " + localStorage.getItem("token")
+        },
+        url: API_BASE_URL + "todo/" + id + "/",
+        method: "delete"
+    })
+        .then(function ({ data, status }) {
+            document.querySelector(`#todo-${id}`).remove();
+        })
+        .catch(function (err) {
+            ErrorToast("An error occurred");
+        });
 }
 
+// function to update the task in todo 
 function updateTask(id) {
-    /**
-     * @todo Complete this function.
-     * @todo 1. Send the request to update the task to the backend server.
-     * @todo 2. Update the task in the dom.
-     */
+    const todoText = document.getElementById("input-button-" + id).value;
+    if (!todoText) {
+        return;
+    }
+    axios({
+        headers: {
+            Authorization: "Token " + localStorage.getItem("token")
+        },
+        url: API_BASE_URL + "todo/" + id + "/",
+        method: "patch",
+        data: { title: todoText }
+    })
+        .then(function ({ data, status }) {
+            document.getElementById("task-" + id).classList.remove("hideme");
+            document.getElementById("task-actions-" + id).classList.remove("hideme");
+            document.getElementById("input-button-" + id).classList.add("hideme");
+            document.getElementById("done-button-" + id).classList.add("hideme");
+            document.getElementById("task-" + id).innerText = todoText;
+        })
+        .catch(function (err) {
+            ErrorToast("An error occurred");
+        });
 }
+
+
+// function to add the new element in todo 
+function newElement(todoText, taskNo) {
+    const availableTasks = document.querySelector(".todo-available-tasks");
+    const newTaskNode = document.createElement("li");
+    newTaskNode.innerHTML = `
+        <input id="input-button-${taskNo}" type="text" class="form-control todo-edit-task-input hideme" placeholder="Edit The Task">
+        <div id="done-button-${taskNo}" class="input-group-append hideme">
+            <button class="btn btn-outline-secondary todo-update-task" type="button" id="update-task-${taskNo}">Done</button>
+        </div>
+    
+        <div id="task-${taskNo}" class="todo-task">
+            ${todoText}
+        </div>
+        <span id="task-actions-${taskNo}">
+            <button style="margin-right:5px;" type="button" id="edit-task-${taskNo}"
+                class="btn btn-outline-warning">
+                <img src="https://res.cloudinary.com/nishantwrp/image/upload/v1587486663/CSOC/edit.png"
+                    width="18px" height="20px">
+            </button>
+            <button type="button" class="btn btn-outline-danger" id="delete-task-${taskNo}">
+                <img src="https://res.cloudinary.com/nishantwrp/image/upload/v1587486661/CSOC/delete.svg"
+                    width="18px" height="22px">
+            </button>
+        </span>`;
+    newTaskNode.id = `todo-${taskNo}`;
+    newTaskNode.classList.add(
+        "list-group-item",
+        "d-flex",
+        "justify-content-between",
+        "align-items-center"
+    );
+    availableTasks.appendChild(newTaskNode);
+
+    document
+        .querySelector(`#edit-task-${taskNo}`)
+        .addEventListener("click", () => editTask(taskNo));
+    document
+        .querySelector(`#update-task-${taskNo}`)
+        .addEventListener("click", () => updateTask(taskNo));
+    document
+        .querySelector(`#delete-task-${taskNo}`)
+        .addEventListener("click", () => deleteTask(taskNo));
+    document.getElementById("input-button-" + taskNo).value = todoText;
+}
+
+const registerButton = document.querySelector("#register-button");
+if (registerButton) registerButton.onclick = register;
+const loginButton = document.querySelector("#login-button");
+if (loginButton) loginButton.onclick = login;
+
+if (localStorage.getItem("token")) {
+    document.querySelector("#logout-button").onclick = logout;
+    document.querySelector("#add-task").onclick = addTask;
+}
+
+// exporting...
+export { editTask, updateTask, deleteTask, newElement };

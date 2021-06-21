67273665b271570d840ee0c46c3a08a8005d998f
Eshaan Agarwal <eshaan060202@gmail.com>
2021-06-21 11:44:29 +0530

    Initial Commit

--- src/init.js
+++ src/init.js
@@ -1,15 +1,33 @@
 import axios from 'axios';
+import { newEntry } from "./main";
 const API_BASE_URL = 'https://todo-app-csoc.herokuapp.com/';
 
 function getTasks() {
-    /***
-     * @todo Fetch the tasks created by the user and display them in the dom.
-     */
+    iziToast.info({
+        title: "Info",
+        message: "Fetching the todos"
+    });
+    axios({
+        headers: {
+            Authorization: "Token " + localStorage.getItem("token")
+        },
+        url: API_BASE_URL + "todo/",
+        method: "get"
+    }).then(function (response) {
+        const { data, status } = response;
+        for (let task of data) {
+            const taskNo = task.id;
+            const taskEntry = task.title;
+            newEntry(taskEntry, taskNo);
+
+        }
+        iziToast.destroy();
+    });
 }
 
 axios({
     headers: {
-        Authorization: 'Token ' + localStorage.getItem('token'),
+        Authorization: "Token " + localStorage.getItem("token"),
     },
     url: API_BASE_URL + 'auth/profile/',
     method: 'get',

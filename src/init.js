8fffaefd0cb1c894743a2e70cddfa18ceb97ac77
Sakshi <sakshinch@gmail.com>
2021-06-21 18:21:14 +0530

    my-commit

--- src/init.js
+++ src/init.js
@@ -1,10 +1,33 @@
 import axios from 'axios';
+import { enterNewData } from './main';
+
 const API_BASE_URL = 'https://todo-app-csoc.herokuapp.com/';
 
 function getTasks() {
     /***
      * @todo Fetch the tasks created by the user and display them in the dom.
      */
+    console.log('fetching tasks')
+    axios({
+        headers: {
+            Authorization: 'Token ' + localStorage.getItem('token'),
+        },
+        url: API_BASE_URL + 'todo/',
+        method: 'get',
+    }).then(function({data, status}) {
+        iziToast.info({
+            title: 'Info',
+            message: 'Loading your profile...'
+        });
+        data.forEach(existingData => {
+            enterNewData(existingData)
+        })
+    }).catch(function(err) {
+        iziToast.error({
+            title: 'Error',
+            message: 'Error occured'
+        })
+    })
 }
 
 axios({

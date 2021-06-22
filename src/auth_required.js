24f51941442f82453fdc07c28f272dccec253d2c
Ujjawal Sharma <ujjawal.sharma.20@gmail.com>
2021-06-22 08:13:59 +0530

    Completed All Tasks

--- src/auth_required.js
+++ src/auth_required.js
@@ -1,8 +1,12 @@
 /***
  * @todo Redirect the user to login page if token is not present.
+<<<<<<< HEAD
  */
 if(!(localStorage.getItem('token')))
 {
     window.location.href='/login/';
 
 }
+=======
+ */
+>>>>>>> parent of bb27c56... fix : redirect requires trailing slash

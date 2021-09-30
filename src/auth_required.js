8d39b103db491591c48ff503f87e14e32454d368
Jaideep Sharma <jaideep.sharma.cse20@itbhu.ac.in>
2021-09-30 10:53:05 +0530

    All features working

--- src/auth_required.js
+++ src/auth_required.js
@@ -1,3 +1,8 @@
 /***
  * @todo Redirect the user to login page if token is not present.
  */
+var accessTokenObj = JSON.parse(localStorage.getItem("token"));
+
+if (!(accessTokenObj >= 6)) {
+  window.location.href = '/login/'
+}
--- src/no_auth_required.js
+++ src/no_auth_required.js
@@ -1,3 +1,8 @@
 /***
  * @todo Redirect the user to main page if token is present.
  */
+var accessTokenObj = JSON.parse(localStorage.getItem("token"));
+
+if (accessTokenObj.length >= 6) {
+  window.location.href = '/'
+}

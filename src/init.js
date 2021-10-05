743129e046060a5eef0ecbb23894afce80cf475d
aayushsrivastava9760 <81965095+aayushsrivastava9760@users.noreply.github.com>
2021-10-05 10:13:14 +0530

    Made changes

--- src/init.js
+++ src/init.js
@@ -1,10 +1,24 @@
 import axios from 'axios';
+import { showTask } from './main';
 const API_BASE_URL = 'https://todo-app-csoc.herokuapp.com/';
 
 function getTasks() {
     /***
      * @todo Fetch the tasks created by the user and display them in the dom.
      */
+    axios({
+        headers: {
+            Authorization: 'Token '+ localStorage.getItem('token'),
+        },
+        url: API_BASE_URL + 'todo/',
+        method: 'get',
+    }).then(function({data, status}){
+        console.log(data);
+        const available_tasks = document.getElementById('');
+        for(let task of data){
+            showTask(task.id,task.title);
+        }
+    })
 }
 
 axios({
